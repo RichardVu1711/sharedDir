ef272c7536fd47b70fb1da7fa4b792365068ebb5
mylord <vuducdung98417@gmail.com>
2023-09-29 13:12:01 +1000

    remove csv reading

--- kernel/calW/GISobs_model.h
+++ kernel/calW/GISobs_model.h
@@ -2,7 +2,6 @@
 #include "../lib/mat_lib.h"
 #include "../lib/global_define.h"
 #include "../lib/GISmsmt_prcs.h"
-#include "../lib/read_write_csv.h"
 
 void GISobs_model(Mat_S* prtcl_X, int step, msmt* msmtinfo, Mat_S* z_cap);
 
--- kernel/calW/ObsJacobian.h
+++ kernel/calW/ObsJacobian.h
@@ -3,7 +3,6 @@
 #include "../lib/mat_lib.h"
 #include "../lib/global_define.h"
 #include "../lib/GISmsmt_prcs.h"
-#include "../lib/read_write_csv.h"
 
 void ObsJacobian(Mat_S* prtcl_X, int step, msmt* msmtinfo, Mat_S* H);
 fixed_type del(fixed_type var, fixed_type s1, fixed_type s2, fixed_type r1, fixed_type r2);
